3531dcc4698ca1615ed668b093ddb4c060766432
alikurniawan <alikoerniawan02@gmail.com>
2019-04-20 09:04:39 +0700

    QC

---
+++ src/Components/graphKpuMain.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+
+
+import CanvasJSReact from '../assets/canvasjs.react';
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+ 
+class AnimatedChart extends Component {
+	render() {
+		const options = {
+			animationEnabled: true,
+			exportEnabled: true,
+			theme: "dark2", // "light1", "dark1", "dark2"
+			title:{
+				text: "Hasil Resmi Lembaga Survey 2019"
+			},
+			data: [{
+				type: "pie",
+				indexLabel: "{label}: {y}%",		
+				startAngle: -90,
+				dataPoints: [
+          { y: 70, label: "02 H Prabowo Subianto" },
+          { y: 30, label: "01 Ir. H Joko Wododo" }
+					// { y: 20, label: "Accomodation" },
+					// { y: 14, label: "Transportation" },
+					// { y: 12, label: "Activities" },
+					// { y: 10, label: "Misc" }	
+				]
+			}]
+		}
+		
+		return (
+		<div>
+      <div className="row">
+        <div className="col">
+        <h1>Komisi Pemilihan Umum 2019</h1>
+          <CanvasJSChart options = {options} 
+            /* onRef={ref => this.chart = ref} */
+          />
+          {/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
+        </div>
+      </div>
+		</div>
+		);
+	}
+}
+
+export default AnimatedChart;    
